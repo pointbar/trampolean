38f04e290ef02ea193be9236c420b2d7d0b0b43e
Pntbr <pntbr@scopyleft.fr>
2014-09-10 17:13:42 +0200

    New session and pass meteor in 0.9.1.1

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 Meteor.startup(function() {
   $('body').attr('data-spy', 'scroll');
   $('body').attr('data-target', '.navbar');
-  $('div.modal-body').append('<a title="Logiciel billetterie en ligne" href="http://www.weezevent.com//?c=sys_widget" class="weezevent-widget-integration" target="_blank" data-src="https://www.weezevent.com/widget_billeterie.php?id_evenement=75827&amp;code=51872&amp;color_primary=00AEEF" data-width="650" data-height="400" data-id="75827" data-resize="0" data-noscroll="0">Billetterie Weezevent</a><script type="text/javascript" src="https://www.weezevent.com/js/widget/min/widget.min.js"></script>');
+  $('div.modal-body').append('<a title="Logiciel billetterie en ligne" href="http://www.weezevent.com//?c=sys_widget" class="weezevent-widget-integration" target="_blank" data-src="https://www.weezevent.com/widget_billeterie.php?id_evenement=80266&amp;code=51872&amp;color_primary=00AEEF" data-width="650" data-height="400" data-id="75827" data-resize="0" data-noscroll="0">Billetterie Weezevent</a><script type="text/javascript" src="https://www.weezevent.com/js/widget/min/widget.min.js"></script>');
 });
 
